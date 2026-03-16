20352227785cd22bebb8809ef8a8ddcb9e3b5244
mhaetinger <mateushaetinger@gmail.com>
2026-03-16 13:05:48 -0300

    exercicio pronto

---
+++ ex1_Aula2/tela/main.py
@@ -0,0 +1,33 @@
+import glfw
+from OpenGL.GL import *
+import random
+import time
+
+if not glfw.init():
+    exit()
+
+window = glfw.create_window(800, 600, "Exercício", None, None)
+if not window:
+    glfw.terminate()
+    exit()
+
+glfw.make_context_current(window)[cite: 335, 336]
+
+
+while not glfw.window_should_close(window):
+
+    glfw.poll_events()
+
+    if glfw.get_key(window, glfw.KEY_ESCAPE) == glfw.PRESS:
+        glfw.set_window_should_close(window, True)
+
+    r, g, b = random.random(), random.random(), random.random()
+    glClearColor(r, g, b, 1.0)[cite: 371]
+
+    glClear(GL_COLOR_BUFFER_BIT)
+
+    glfw.swap_buffers(window)
+
+    time.sleep(0.5)
+
+glfw.terminate()
